65fd16f615ff68c60e453466579f021dbdb12b13
ricar <ricardomar06@hotmail.com>
2024-12-22 12:43:23 -0600

    menos balloons

--- BalloonGame/BalloonConstants.py
+++ BalloonGame/BalloonConstants.py
@@ -1,5 +1,5 @@
 # Constants used by more than one Python module
 
-N_BALLONS = 200  # Number of balloons in a round of the game
+N_BALLONS = 100  # Number of balloons in a round of the game
 BALLOON_MISSED = "Missed"  # balloon went off the top
 BALLOON_MOVING = "Balloon Moving"  # balloon is moving
